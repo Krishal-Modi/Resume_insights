bea1ea162d74f8de3f0affa8a4b2be1082af946f
krishal-modi <111569458+Krishal-Modi@users.noreply.github.com>
2024-10-22 14:07:23 +0530

    Add files via upload

--- Resumeinsights/resumedetails/static/js/main.js
+++ Resumeinsights/resumedetails/static/js/main.js
@@ -13,3 +13,19 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 
 
 
+// Footer links smooth scrolling effect
+
+
+document.querySelectorAll('.footer-links a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+        if (targetElement) {
+            window.scrollTo({
+                top: targetElement.offsetTop,
+                behavior: 'smooth'
+            });
+        }
+    });
+});
